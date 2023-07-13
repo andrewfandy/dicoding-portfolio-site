7cbfb06aaccba80d620d39427d28b32fcb92a5b3
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2023-07-13 20:52:15 +0700

    Adding new repo

---
+++ main.js
@@ -0,0 +1,30 @@
+// Mendapatkan semua elemen nav-item
+let navItems = document.querySelectorAll('.nav-item a');
+
+// Mendapatkan semua section
+let sections = document.querySelectorAll('section');
+
+// Fungsi untuk menghapus kelas 'active' dari semua nav-item
+function removeActiveClasses() {
+    navItems.forEach((navItem) => {
+        navItem.parentElement.classList.remove('active');
+    });
+}
+
+// Fungsi untuk menambahkan kelas 'active' ke nav-item yang sesuai
+function setActiveClass() {
+    let index = sections.length;
+
+    while(--index && window.scrollY + 200 < sections[index].offsetTop) {}
+
+    navItems.forEach((navItem) => navItem.parentElement.classList.remove('active'));
+    navItems[index].parentElement.classList.add('active');
+}
+
+// Menjalankan fungsi setActiveClass saat window di-scroll
+window.addEventListener('scroll', function() {
+    setActiveClass();
+});
+
+
+
